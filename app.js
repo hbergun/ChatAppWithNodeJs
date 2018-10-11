039c8038fe2cbed077720fa941ea183b601f048b
Berk Ergün <hasan.berk.ergun@hotmail.com>
2018-10-12 01:53:08 +0300

    Version 0.0.3

--- app.js
+++ app.js
@@ -10,18 +10,26 @@ const io=socketio.listen(server); //Server 4000-i Socket.io Serveri'dinliyor
 
 io.sockets.on('connection',(socket)=>{ //Socket Page'in Bize State Bildirimi
     console.log(socket.id+" ID 'Connected"); //Connection Durumunda Start
-    console.log(socket);
+    //console.log(socket);
 
     socket.on('sendHello',(datas)=>{ //2.Paramtere Geçildiği İçin Alabildik.
         console.log('Client Says SendHello');
-        console.log(datas);
-
-        });
-        setTimeout(()=>{
+        console.log(datas.city); //Datas Object Type
+         });
+       /* });
+        setInterval(()=>{
             socket.emit('SayHello');
-        },2500);
-
+        },50000);*/
+        
+        socket.on('newUser',(data)=>{
+            console.log(data.name+" Chat'e Katıldı");
+            socket.broadcast.emit('OtherUser',{name:data.name});
+        });
+    
     socket.on('disconnect',()=>{
     console.log(socket.id+" ID 'Disconnected"); //Disconnect Durumunda Stop
-    });
+    socket.broadcast.emit('userDisconnect',{name:global.name}); //??
+        });
+        
+   
 });
