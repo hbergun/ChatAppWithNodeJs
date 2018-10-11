473f02d3ba67d86122fec558928b72d14854ea6b
Berk Ergün <hasan.berk.ergun@hotmail.com>
2018-10-11 22:37:38 +0300

    Stable V 0.0.2

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ server.listen(4000); //4000 Port
 const io=socketio.listen(server); //Server 4000-i Socket.io Serveri'dinliyor
 
 io.sockets.on('connection',(socket)=>{ //Socket Page'in Bize State Bildirimi
-    console.log(socket.id+" ID 'Connected"); //Connection Durumunda 
+    console.log(socket.id+" ID 'Connected"); //Connection Durumunda Start
+    console.log(socket);
+
+    socket.on('sendHello',(datas)=>{ //2.Paramtere Geçildiği İçin Alabildik.
+        console.log('Client Says SendHello');
+        console.log(datas);
+
+        });
+        setTimeout(()=>{
+            socket.emit('SayHello');
+        },2500);
+
     socket.on('disconnect',()=>{
-    console.log(socket.id+" ID 'Disconnected"); //Disconnect Durumunda 
+    console.log(socket.id+" ID 'Disconnected"); //Disconnect Durumunda Stop
     });
 });
